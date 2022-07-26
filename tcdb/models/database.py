90d0228b1074cceb8008950dc0d2477478a29af1
JJ <jj.miller.jm@gmail.com>
2022-07-26 18:57:26 +0000

    move get_engine function to database.py

--- tcdb/models/database.py
+++ tcdb/models/database.py
@@ -1,7 +1,18 @@
-from sqlalchemy import create_engine
-from tcdb.config import settings
 
-def get_engine():
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from tcdb.config import settings
+from tcdb.models import (
+    Region,
+    Storm,
+    Observation,
+    Forecast,
+    Step,
+    Track,
+)
+
+def getEngine():
 
     engine = create_engine(
         f"mysql+mysqlconnector://{settings.db.get('USER')}:{settings.db.get('PASS')}@{settings.db.get('HOST')}:{settings.db.get('PORT')}/{settings.db.get('SCHEMA')}"
