4cad1c1b3af9b8b4a180ef9a9c60080a37c265f6
JJ <jj.miller.jm@gmail.com>
2022-12-21 20:32:13 +0000

    Stop tracking files with sensative information and update bash scripts to use variables from .env file

--- tcdb/pipeline/master_process_storms.sh
+++ tcdb/pipeline/master_process_storms.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-project_dir='/home/jmiller/tcdb'
+. ../.env # export environment variables
+
+project_dir=${TCDB_HOME}
 mysql_cmd=`which mysql`
 tmp_dir='/Work_Data/tcdb/data/tmp'
 
-source "${project_dir}/db/scripts/.env_vars"
-${mysql_cmd} -u ${MYSQL_USER} -pb14z3r5 -h ${MYSQL_HOST} tc --batch --raw < ${project_dir}/db/scripts/getSeasonStorms.sql > ${tmp_dir}/storms.tsv
+#${mysql_cmd} -u ${TCDB_USER} -p${TCDB_PW} -h ${TCDB_HOST} tc --batch --raw < ${project_dir}/db/scripts/getSeasonStorms.sql > ${tmp_dir}/storms.tsv
 
 #cd /home/jmiller/tcdb/tcdb/pipeline
 
